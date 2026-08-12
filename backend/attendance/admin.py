3a9d89ac497b4b6dfdae88c44b23e44fdadfa31e
Aman <aman08.stars@gmail.com>
2026-08-12 07:24:15 +0530

    feat: register ActivityLog in admin

--- backend/attendance/admin.py
+++ backend/attendance/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from attendance.models import AttendanceSession, QRToken, Attendance
+from attendance.models import AttendanceSession, QRToken, Attendance, ActivityLog
 
 admin.site.register(AttendanceSession)
 admin.site.register(QRToken)
 admin.site.register(Attendance)
+admin.site.register(ActivityLog)
