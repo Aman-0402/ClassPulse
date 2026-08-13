817642f8c588afedd893774704886dc9c4619af8
Aman <aman08.stars@gmail.com>
2026-08-13 16:11:59 +0530

    feat: section-wise student list in Django admin (list_display, filter, search)

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from accounts.models import User, StudentProfile
 
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ("crn", "user", "get_full_name", "course", "semester", "section")
+    list_filter = ("section", "course", "semester")
+    search_fields = ("crn", "user__username", "user__first_name")
+    ordering = ("section", "crn")
+
+    @admin.display(description="Name")
+    def get_full_name(self, obj):
+        return obj.user.get_full_name() or obj.user.username
+
+
 admin.site.register(User)
-admin.site.register(StudentProfile)
